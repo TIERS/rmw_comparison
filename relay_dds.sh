719ff045d0f8e368d09512af6481a876d31c1a4c
JiaqiangZhang <jizhan@utu.fi>
2023-05-09 19:54:02 +0300

    modify relay_dds.sh

--- relay_dds.sh
+++ relay_dds.sh
@@ -3,31 +3,34 @@
 set -e 
 
 . /opt/ros/galactic/setup.bash
-. ../install/setup.bash
+. ./install/setup.bash
 . /opt/ros/galactic/setup.bash
-export ROS_DOMAIN_ID=7
+# export ROS_DOMAIN_ID=0
 
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-roundtrip_mode="Relay"
+# roundtrip_mode="Relay"
+net="wifi" # "ethernet"
 
 
-for msg in Array1k Array4k Array16k Array256k Array1m PointCloud512k PointCloud1m PointCloud2m PointCloud4m PointCloud8m
+for msg in Array1k Array4k Array16k Array64k Array256k Array1m Array2m PointCloud512k PointCloud1m PointCloud2m
 do
-    echo 'testing $msg'
+    echo testing $msg
+    RMW_IMPLEMENTATION=rmw_cyclonedds_cpp ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/${net}_cyclonedds_${msg}_r10_pub_2laptops_Relay.csv --roundtrip-mode Relay &
     ssh jqzhang@192.168.50.49 "cd /home/jqzhang/workspace/tracing_ws; . /opt/ros/galactic/setup.bash; . install/setup.bash; \
-    ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/log_fastdds_${msg}_r10_sub_2laptops_Main.csv --reliability RELIABLE --roundtrip-mode Main" &
-    ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/log_fastdds_${msg}_r10_pub_2laptops_Relay.csv --reliability RELIABLE --roundtrip-mode Relay
-    echo 'sleep 3s'
+    RMW_IMPLEMENTATION=rmw_cyclonedds_cpp ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/${net}_cyclonedds_${msg}_r10_sub_2laptops_Main.csv --roundtrip-mode Main" 
+    echo sleep 3s
     sleep 3
 done
 
-# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
-# for msg in Array1k Array4k Array16k Array256k Array1m PointCloud512k PointCloud1m PointCloud2m PointCloud4m PointCloud8m
-# do
-#     ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 20 -m $msg --max-runtime 30 --logfile experiment/log_cyclonedds_${msg}_pub_2laptops_${roundtrip_mode}.csv --reliability RELIABLE --roundtrip-mode ${roundtrip_mode}
-#     sleep 5
-# done
+for msg in Array1k Array4k Array16k Array64k Array256k Array1m Array2m PointCloud512k PointCloud1m PointCloud2m
+do
+    echo testing $msg
+    RMW_IMPLEMENTATION=rmw_fastrtps_cpp ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/${net}_fastdds_${msg}_r10_pub_2laptops_Relay.csv --reliability RELIABLE --roundtrip-mode Relay &
+    ssh jqzhang@192.168.50.49 "cd /home/jqzhang/workspace/tracing_ws; . /opt/ros/galactic/setup.bash; . install/setup.bash; \
+    RMW_IMPLEMENTATION=rmw_fastrtps_cpp ./install/performance_test/lib/performance_test/perf_test -c rclcpp-single-threaded-executor -r 10 -m $msg --max-runtime 30 --logfile experiment/${net}_fastdds_${msg}_r10_sub_2laptops_Main.csv --reliability RELIABLE --roundtrip-mode Main"
+    echo sleep 3s
+    sleep 3
+done
 
 # export ROS_LOCALHOST_ONLY=1
 
